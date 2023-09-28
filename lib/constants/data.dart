acf5dd8e3937b1f970b6d15eea2fb5f83f5f8ef4
ian-usm <135771194+ian-usm@users.noreply.github.com>
2023-09-27 22:23:14 -0300

    Update data.dart

--- lib/constants/data.dart
+++ lib/constants/data.dart
@@ -2,6 +2,6 @@ const videos = [
   {'miniatura':'miniatura1.jpg'},
   {'miniatura':'miniatura2.jpg'},
   {'miniatura':'miniatura3.jpg'},
-  {'miniatura':'miniatura3.jpg'},
+  {'miniatura':'miniatura4.jpg'},
   {'miniatura':'miniatura3.jpg'},
 ];
